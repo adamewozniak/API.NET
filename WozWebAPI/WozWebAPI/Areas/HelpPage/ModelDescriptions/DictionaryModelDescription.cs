a5e958bc9d9e18c09ab9d5076f66f20fdccb6947
Adam Wozniak <adam.e.wozniak@gmail.com>
2018-09-16 23:50:36 -0700

    Swagger

---
+++ WozWebAPI/WozWebAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WozWebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ WozWebAPI/WozWebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WozWebAPI.WebApiApplication" Language="C#" %>
